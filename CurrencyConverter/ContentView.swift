e714021d32f0acf440363e1539ddc096abd7cee4
moein barzegaran <barzegaran.m@gmail.com>
2020-01-12 12:40:54 +0330

    Initial Commit

---
+++ CurrencyConverter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CurrencyConverter
+//
+//  Created by Moein Barzegaran on 1/12/20.
+//  Copyright © 2020 Moein Barzegaran. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
